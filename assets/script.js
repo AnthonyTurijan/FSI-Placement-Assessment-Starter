2ffa93d0afb8d3ed985fa03c6b04e7a7b1cc0d96
AnthonyTurijan <anthonyturijan@gmail.com>
2021-12-02 21:25:26 -0800

    meow 2

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@
 let yourName = "Anthony Turijan" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
+
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
@@ -16,5 +17,61 @@ function updateGinger(displayGinger){
 document.getElementById('add-gb').addEventListener('click', function() {
     gb++
     updateGinger(`${gb}`)
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
 })
+
+// Event listener for clicks on the "-" button for ginger bread cookie
+document.getElementById('minus-gb').addEventListener('click', function() {
+    if (gb > 0){
+        gb--
+    }
+    updateGinger(`${gb}`)
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+// Chocolate Chip Code
+
+function updateChocolate(displayChocolate){
+    let chocolate = document.querySelector('#qty-cc')
+    chocolate.innerHTML = displayChocolate
+}
+
+//Event Lister Plus and Minus Chocolate Chip
+
+document.getElementById('add-cc').addEventListener('click', function(){
+    cc++
+    updateChocolate(`${cc}`)
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    if (cc > 0){
+        cc--
+    }
+    updateChocolate(`${cc}`)
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+// Sugar Sprinkle Code
+
+function updateSugar(displaySugar){
+    let sugar = document.querySelector('#qty-sugar')
+    sugar.innerHTML = displaySugar
+}
+
+document.getElementById('add-sugar').addEventListener('click', function(){
+    sugar++
+    updateSugar(`${sugar}`)
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    if (sugar > 0){
+        sugar--
+    }
+    updateSugar(`${sugar}`)
+    document.getElementById('qty-total').textContent=gb+cc+sugar;
+})
+// Total Quantity Code
+//document.getElementById('qty-total').innerHTML = 1
 
